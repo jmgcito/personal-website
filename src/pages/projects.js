3b3968e85f65c7fefb725a1e8a02c7436ee99a3c
jmgcito <joshuamguillen@gmail.com>
2022-11-29 19:17:03 -0800

    Added responsive gifs to project section with working links.

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,39 +1,37 @@
 import React from "react";
 import "../styles/projects.css";
 import PixcassoGif from "../gifs/pixcasso.gif";
+import BattleshipGif from "../gifs/battleship.gif";
+import BudgeterGif from "../gifs/Budgeter.gif";
+import GetWeatherGif from "../gifs/getWeather demo.gif";
+import TriangleGif from "../gifs/Impossible Triangle.gif";
+import KeyboardGif from "../gifs/keyboard demo.gif";
 
-const Project = () => {
+const gifs = [
+  { gif: PixcassoGif, url: "https:github.com/jmgcito/Pixcasso" },
+  { gif: GetWeatherGif, url: "https://github.com/jmgcito/getWeather" },
+  { gif: KeyboardGif, url: "https://github.com/jmgcito/virtual-keyboard" },
+  { gif: BattleshipGif, url: "https://github.com/jmgcito/battleship" },
+  { gif: BudgeterGif, url: "https://github.com/jmgcito/budgeter" },
+  { gif: TriangleGif, url: "https://github.com/jmgcito/impossible-triangle" },
+];
+
+const Project = (props) => {
+  console.log(props.url);
   return (
     <div className="project">
-      <img className="gif" src={PixcassoGif}></img>
-
-      {/* <p>
-        <strong>Project title</strong>
-        <br></br>
-        short description of this thing
-      </p> */}
+      <a href={props.url} target="_blank">
+        <img className="gif" src={props.gif}></img>
+      </a>
     </div>
   );
 };
 
 const Projects = () => {
+  const thumbnails = gifs.map((g) => <Project gif={g.gif} url={g.url} />);
   return (
     <div id="projects">
-      <div className="under-construction">
-        <span>
-          This section is currently under construction. :,(<br></br> Pretty
-          please come back another day.
-        </span>
-      </div>
-      <div id="project-thumbnails">
-        {" "}
-        <Project />
-        <Project />
-        <Project />
-        <Project />
-        <Project />
-        <Project />
-      </div>
+      <div id="project-thumbnails">{thumbnails}</div>
       {/* <a>
         <button>More Projects</button>
       </a> */}
